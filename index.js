313243df624196f92638c06f2cf476964ee4590c
Gyginee <hungnguyenminh66@gmail.com>
2024-04-12 17:06:39 +0700

    update

--- index.js
+++ index.js
@@ -56,5 +56,5 @@ const server = http.createServer((req, res) => {
 }); */
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    //console.log(`Server is running on port ${PORT}`);
 });
