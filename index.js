f0816e57c9d87a2e1c2ed84f4910cc2ff7b6316f
Gyginee <nguyenminhhung.work@gmail.com>
2025-03-16 17:46:40 +0700

    done

--- index.js
+++ index.js
@@ -59,5 +59,7 @@ app.listen(PORT, () => {
     //console.log(`Server is running on port ${PORT}`);
 });
 */
-
+app.listen(PORT, () => {
+    //console.log(`Server is running on port ${PORT}`);
+});
 module.exports = app;
